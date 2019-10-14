12c4a8d7c1c97dbebc0c7a6acb7c56f182cce37e
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-10-13 15:22:32 -1000

    Finished objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -17,7 +17,10 @@ const dino1 = {
   diet: "carnivorous",
   weight: "7000kg",
   length: "12m",
-  period: "Late Cretaceous"
+  period: "Late Cretaceous",
+  roar: function () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
 }
 
 const dino2 = {
@@ -39,20 +42,20 @@ const dino3 = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dino1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dino3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dino1.roar());
 
 
 // ==== Arrays ====
